94144efa0d4dbe3948d14ed54fd287c6ce75d5fd
SarahHeshamNadrin <34913032+SarahHeshamNadrin@users.noreply.github.com>
2018-01-27 20:27:15 +0200

    Update and rename navbar_admin.html to navbar_admin.php

--- navbar_admin.php
+++ navbar_admin.php
@@ -3,19 +3,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse ">
   <div class="container">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="Home_admin.html">Home</a></li>
+      <li><a href="Home_admin.php">Home</a></li>
       <li><a href="#">Products</a></li>
       <li><a href="#">Users</a></li>
       <li><a href="#">Manual Order</a></li>
