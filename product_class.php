3cf96b5a6bb84562b04c106e956e8c32d4d7d119
Ahmed-Hamdy2018 <34912594+Ahmed-Hamdy2018@users.noreply.github.com>
2018-01-28 00:49:37 +0200

    Add files via upload

--- product_class.php
+++ product_class.php
@@ -33,7 +33,7 @@ class Product {
      $data = $stmt->fetch(PDO::FETCH_ASSOC);
      return $data;
        }
-//update specific product details by product id
+        //update specific product details by product id
        public function update($product_ID,$product_name,$price,$category,$image,$status){
          global  $conn;
           $query = "UPDATE product
@@ -46,48 +46,95 @@ class Product {
 
         //insert new product in table product
         public function insertData($product_name,$price,$category,$image,$status){
+          global  $conn;
          $query = "INSERT INTO product SET product_name='$product_name', price ='$price' , category='$category', image='$image', status='$status' ";
          $stmt = $conn->prepare($query);
-         $stmt->execute();
+        $stmt->execute();
          return true;
          }
 
         //delete some product of table product by product id
         public function deleteData($product_ID){
+            global  $conn;
         $query="DELETE FROM product WHERE product_ID='$product_ID' ";
         $stmt = $conn->prepare($query);
         $stmt->execute();
         return true;
         }
+          //select table orders admin from user and orders
+          public function select_order(){
+              global  $conn;
+          $query="SELECT orders.user_id,concat(orders.date ,' ',orders.time) as order_date ,user.user_name ,user.Room_no,user.Ext,orders.status FROM orders,
+          user WHERE orders.user_id = user.user_ID";
+          $stmt = $conn->query($query) or die("failed!");
+          while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+          $data[]=$row;
+
+          }
+          return $data;
+
+          }
+          //select table product of order admin from user and orders
+          public function select_product_order(){
+              global  $conn;
+          $query="SELECT orders.user_id,concat(orders.date ,' ',orders.time) as order_date ,user.user_name ,user.Room_no,user.Ext,orders.status FROM orders,
+          user WHERE orders.user_id = user.user_ID";
+          $stmt = $conn->query($query) or die("failed!");
+          while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+          $data[]=$row;
+
+          }
+          return $data;
+
+          }
+          //update specific product status
+         public function update($product_name,$status){
+           global  $conn;
+            $query = "UPDATE product
+             SET status='$status'
+             WHERE product_name ='$product_name' ";
+             $stmt = $conn->prepare($query);
+             $stmt->execute();
+             return true;
+            }
+
+
 
 
       }
 
 
 
-//how to use function
+// //how to use function
+//
 
-$p = new product(); //create object of class
-// loop for all column in table
-foreach($p->showData() as $value){
- extract($value);
+ $p = new product(); //create object of class
 
-
-echo "<br> no:".$product_ID;
-echo "<br> name:".$product_name;
-echo "<br> price:".$price;
-echo "<br> category:".$category;
-
-}
-
-//get product by id
-extract($p->getById(1));
-echo "<br><br> specific product ";
-echo "<br> no:".$product_ID;
-echo "<br> name:".$product_name;
-echo "<br> price:".$price;
-echo "<br> category:".$category;
-
+// // loop for all column in table
+// foreach($p->select_order() as $value){
+//  extract($value);
+// echo "<br> no:".$user_id;
+// echo "<br> name:".$user_name;
+// echo "<br> price:".$Room_no;
+// echo "<br> category:".$Ext;
+//
+// }
+//
+// //get product by id
+// extract($p->getById(1));
+// echo "<br><br> specific product ";
+// echo "<br> no:".$product_ID;
+// echo "<br> name:".$product_name;
+// echo "<br> price:".$price;
+// echo "<br> category:".$category;
+//
+//
+// //deleteData
+// $p->deleteData(10);
+// $p->deleteData(11);
+// $p->deleteData(12);
+// //update class
+// $p->update(7,"juse",5,"cool-drink","ayklam","avaliable");
 
 
  ?>
