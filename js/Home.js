0d94cb1d9a1f6dd0d7874d90f5c62ed25b06f3f0
SarahHeshamNadrin <34913032+SarahHeshamNadrin@users.noreply.github.com>
2018-01-27 20:32:45 +0200

    Update Home.js

--- js/Home.js
+++ js/Home.js
@@ -1,38 +1,65 @@
 
-images=['img/coffee.png','img/cola.png','img/water.png','img/water.png','img/water.png']
-product=['Coffee','Cola','Water','Water','Water']
-var price_num=8
-var product_list=document.getElementById('product_list')
+var data_2d = DATA.split("\n").map(function(e) {
+    return e.split(",").map(String);
+})
+data_2d.splice(2, 1);
+var row=data_2d.length
+var col=data_2d[0].length
+images=[]
 
+
+var product_list=document.getElementById('product_list')
 var reciept=document.getElementById('reciept')
 var label_notes=document.getElementById('label_notes')
-//var product_prices=document.getElementById('product_prices')
+
 window.addEventListener('load',function(){
 
-
- for(var i=0;i<5;i++){
-
+ 
+ for(var i=0;i<row;i++){
+   
     var element=document.createElement('div')
     element.className="product_list_element"
- 	var img=document.createElement('img')
- 	img.src=images[i]
- 	img.className="product_photo"
-     
-
-    img.addEventListener('click',function()
-   {
-      
-    var element=document.createElement('div')
+ 	  var img=document.createElement('img')
+ 	  img.src=data_2d[i][3]
+    img.id=data_2d[i][0]
+ 	  img.className="product_photo"
+    images.push(img)
     
- 	//product_prices.parentNode.insertBefore(img,product_prices)
+    
+    
+ 	  element.appendChild(img)
+ 	
     var product_name=document.createElement('p')
-    product_name.innerText=product[0] + "  "
-    //product_name.innerText += " " //
+    product_name.className="label label-primary"
+    product_name.innerText=data_2d[i][1]
+  
+    var price=document.createElement('span')
+    price.className="badge"
+    price.innerText=data_2d[i][2] + " EGP"
+    product_name.appendChild(price)
+  
+    element.appendChild(product_name)
+    product_list.appendChild(element)
+    
+ }
+
+
+images.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+     
+    var element=document.createElement('div')
+    element.id="product" +elem.id
+
+    
+    var product_name=document.createElement('p')
+    product_name.innerText=data_2d[elem.id-1][1] + "  "
     product_name.className="element_order"
+    product_name.name="product_name"+elem.id
     element.appendChild(product_name)
 
+
     var product_quantity=document.createElement('input')
-    product_quantity.name="product_quantity"
+    product_quantity.name="product_quantity"+elem.id
     product_quantity.type="text"
     product_quantity.value=1
     product_quantity.style.width="50px"
@@ -40,15 +67,15 @@ window.addEventListener('load',function(){
 
     var price=document.createElement('p')
     price.className="element_order"
-    price.innerText=8
-   
+    price.innerText=data_2d[elem.id-1][2]
+    price.name="price"+elem.id
 
     var product_add=document.createElement('span')
     product_add.className="glyphicon glyphicon-plus"
     product_add.addEventListener('click',function()
     {
       product_quantity.value++ ;
-      price.innerText=parseInt(price.innerText)+price_num
+      price.innerText=parseInt(price.innerText)+parseInt(data_2d[elem.id-1][2])
       
     })
 
@@ -60,9 +87,9 @@ window.addEventListener('load',function(){
     {
       if(product_quantity.value > 1)
       {
-      	product_quantity.value-- ;
-        price.innerText=price.innerText-price_num
-      }	
+        product_quantity.value-- ;
+        price.innerText=price.innerText-parseInt(data_2d[elem.id-1][2])
+      } 
       
 
     })
@@ -91,24 +118,9 @@ window.addEventListener('load',function(){
 
    reciept.insertBefore(element,reciept.firstChild)
 
-   })
-
- 	element.appendChild(img)
- 	//
-    var product_name=document.createElement('p')
-    product_name.className="label label-primary"
-    product_name.innerText=product[i]
-    var price=document.createElement('span')
-    price.className="badge"
-   // price.className +="price"
-    price.innerText="7 L.E"
-    product_name.appendChild(price)
-    element.appendChild(product_name)
-    product_list.appendChild(element)
-    
- }
-
+   
+    });
+});
 
 
 })
-
